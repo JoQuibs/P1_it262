a47fc245d1564684e8b9f3d91e61df1b349d71d6
Isaac J <duckhunterjr@gmail.com>
2023-01-17 19:52:29 -0800

    add base php and html

--- P1_262/index.php
+++ P1_262/index.php
@@ -1,3 +1,20 @@
+<?php
+//initalize variables
+
+
+//check server request
+if($_SERVER['REQUEST_METHOD'] =='POST') {
+    //Check POST associate array
+    empty($_POST['userInput']) ? : $_POST['userInput'] ; //This is a Ternary operator, condition ? true:false
+    //if both toggle empty, print warning
+   
+
+
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,33 +22,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Group Project</title>
-    <link href="css/styles.css" type="text/css" rel="stylesheet">
+    <link href="./css/styles.css" type="text/css" rel="stylesheet">
 </head>
 <body>
-    <header>
-    <h1><a href="index.php">Temperature Converter</a></h1>
-        <nav>
-            <ul>
-                <li><a href="celcius.php">C-F</a></li>
-                <li><a href="far.php">F-C</a></li>
-                <li><a href="kel.php">K-C</a></li>
-            </ul>
-        </nav>
-    </header>
-    <footer>
-        <ul>
-            <li>Copyright &copy;
-                2022</li>
-            <li>All Rights Reserved</li>
-            <li><a href="./index.php">Web Design by Jomar Quiban</a></li>
-            <li><a id="html-checker" href="#">HTML Validation</a></li>
-            <li><a id="css-checker" href="#">CSS Validation</a></li>
-            </ul>
-            
-            <script>
-                    document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
-                    document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
-            </script>
-    </footer>
+    <form action="" method="POST">
+    <p>Input Number: <input type="number" name="userInput" value="<?php if(isset($_POST['userInput'])) echo $_POST['userInput'];?>"/></p>
+<!-- Temp toggle container -->
+<div>
+    <!-- Temp A here  -->
+    <div>
+        <p><input type="radio" name="tempA" value="fahr" /> Fahrenheit </p>
+        <p><input type="radio" name="tempA" value="cel" /> Celsius</p>
+        <p><input type="radio" name="tempA" value="kel" /> Kelvin</p>
+    </div>
+    <div>
+        <p>Convert To</p>
+    </div>
+    <!-- Temp B here -->
+    <div>
+        <p><input type="radio" name="tempB" value="fahr" /> Fahrenheit </p>
+        <p><input type="radio" name="tempB" value="cel" /> Celsius</p>
+        <p><input type="radio" name="tempB" value="kel" /> Kelvin</p>
+    </div>
+</div>
+    <p><input type="submit"/></p>
+    </form>
+    <?php
+    echo '<pre>';
+    echo var_dump($_POST);
+    
+    echo'</pre>'
+    
+    
+    ?>
+    
 </body>
 </html>
