735ab638fc32ad825dfd85507eae45838bbb5fc7
Isaac J <duckhunterjr@gmail.com>
2023-01-19 20:40:12 -0800

    add working form with proper checks

--- P1_262/index.php
+++ P1_262/index.php
@@ -4,45 +4,133 @@ $input = '';
 $conversion = '';
 $msg = '';
 
-#------set conversion functions
-function fahrToCel($arg) {
-    return $arg .' executed from fahrToCell function';
+function fahrToCel($arg)
+{
+    $num = $arg;
+    $int_val = intval($num);
+    $conversion = (($int_val - 32) * 5) / 9;
+    return $conversion;
+}
+
+function fahrToKel($arg)
+{
+    $num = $arg;
+    $int_val = intval($num);
+    $conversion = (($int_val - 32) * 5) / 9 + 273.15;
+    return $conversion;
+}
+
+function celToKel($arg)
+{
+    $num = $arg;
+    $int_val = intval($num);
+    $conversion = $int_val + 273.15;
+    return $conversion;
+}
+
+function celToFahr($arg)
+{
+    $num = $arg;
+    $int_val = intval($num);
+    $conversion = ($int_val * 9) / 5 + 32;
+    return $conversion;
+}
+
+function kelToFahr($arg)
+{
+    $num = $arg;
+    $int_val = intval($num);
+    $conversion = (($int_val - 273.15) * 9) / 5 + 32;
+    return $conversion;
+}
+
+function kelToCel($arg)
+{
+    $num = $arg;
+    $int_val = intval($num);
+    $conversion = $int_val - 273.15;
+    return $conversion;
+}
+
+function conversionMsg($input, $conversion)
+{
+    return '<p class = "result">The conversion of <b>' .
+        $input .
+        '</b> from <b>' .
+        strtoupper($_POST['tempA'][0]) .
+        '°</b> to <b>' .
+        strtoupper($_POST['tempB'][0]) .
+        '°</b> is equal to <b>' .
+        number_format((float) $conversion, 2) .
+        '</b> degrees!</p>';
 }
 
 //check server request
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //Check POST associate array
-    if (!empty($_POST['userInput'])) {
+    if (empty($_POST['userInput'])) {
+        $msg = 'Please input a value';
+    } else {
         $input = intval($_POST['userInput']);
-    } //This is a Ternary operator, condition ? true:false
 
-    //if both toggle empty, print warning
-    if (!array_key_exists('tempA',$_POST) && !array_key_exists('tempB',$_POST)) {
-        $msg = 'Please pick two temperatures to convert';
-
-    } else {//keys exist
-     
-        //if one is toggled and not the other, print warning
-    if (isset($_POST['tempA']) && !isset($_POST['tempB'])) {
-        $msg = 'Please pick a temperature type ';
-    }
-    if (!isset($_POST['tempA']) && isset($_POST['tempB'])) {
-        $msg = 'Please pick a temperature to convert';
-    }
-
-    //if tempA and tempB are the same
-    if($_POST['tempA']=='fahr' && $_POST['tempB']=='fahr' ) {
-        # Here is where we place all formulas and save to conversion--------------------
-        if(empty($input)) {
-            $msg= 'input a value';
+        //if both toggle empty, print warning
+        if (
+            !array_key_exists('tempA', $_POST) &&
+            !array_key_exists('tempB', $_POST)
+        ) {
+            $msg = 'Please pick two temperatures to convert';
         } else {
-            //
-            $conversion = fahrToCel($input);
-        }
-        
-    }
-    }//end else
-}//
+            //keys exists
+            //if one is toggled and not the other, print warning
+            if (isset($_POST['tempA']) && !isset($_POST['tempB'])) {
+                $msg = 'Please pick a temperature type ';
+            } elseif (!isset($_POST['tempA']) && isset($_POST['tempB'])) {
+                $msg = 'Please pick a temperature to convert';
+            } else {
+                //temp combo options
+                if ($_POST['tempA'] == 'fahr' && $_POST['tempB'] == 'cel') {
+                    $conversion = fahrToCel($input);
+                    $msg = conversionMsg($input, $conversion);
+                } elseif (
+                    $_POST['tempA'] == 'fahr' &&
+                    $_POST['tempB'] == 'kel'
+                ) {
+                    $conversion = fahrToKel($input);
+                    $msg = conversionMsg($input, $conversion);
+                } elseif (
+                    $_POST['tempA'] == 'cel' &&
+                    $_POST['tempB'] == 'fahr'
+                ) {
+                    $conversion = celToFahr($input);
+                    $msg = conversionMsg($input, $conversion);
+                } elseif (
+                    $_POST['tempA'] == 'cel' &&
+                    $_POST['tempB'] == 'kel'
+                ) {
+                    $conversion = celToKel($input);
+                    $msg = conversionMsg($input, $conversion);
+                } elseif (
+                    $_POST['tempA'] == 'kel' &&
+                    $_POST['tempB'] == 'fahr'
+                ) {
+                    $conversion = kelToFahr($input);
+                    $msg = conversionMsg($input, $conversion);
+                } elseif (
+                    $_POST['tempA'] == 'kel' &&
+                    $_POST['tempB'] == 'cel'
+                ) {
+                    $conversion = kelToCel($input);
+                    $msg = conversionMsg($input, $conversion);
+                } else {
+                    // temp same, please try again
+                    $msg = conversionMsg($input, $input);
+                }
+            } 
+        } 
+    } 
+}
+
+//end server request
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -85,8 +173,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <?php
     echo '<pre>';
     echo var_dump($_POST);
-    echo var_dump($input);
-    echo var_dump($msg);
+    echo var_dump($input) . 'input--line 140 <br>';
+    echo var_dump($msg) . 'msg==line141     ';
     echo '</pre>';
     echo $conversion;
     ?>
